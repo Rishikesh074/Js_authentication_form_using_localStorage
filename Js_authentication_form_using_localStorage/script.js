b0e05308843af65f8d256188867b2ec286927536
Rishikesh074 <rishikeshvr074@gmail.com>
2022-02-28 11:41:43 +0530

    Code corrections

--- Js_authentication_form_using_localStorage/script.js
+++ Js_authentication_form_using_localStorage/script.js
@@ -1,38 +1,76 @@
-var modal = document.getElementsByClassName('signup_section');
+var modal = document.querySelector('.signup_section');
         var Iname = document.getElementById('name');
         var pw = document.getElementById('pw');
         let personData = [];
-        var storedData = localStorage.getItem('Data'); 
-     
+        var storedData = localStorage.getItem('Data');
         personData = [...(JSON.parse(storedData))];
 
-console.log(personData);
+        console.log(personData);
 
-function store() {
-    var storedData = localStorage.getItem('Data'); 
-    let obj = {
-        uname: Iname.value,
-        password: pw.value,
-    }
-    personData.push(obj);
+        var confirm_password = document.getElementById("confirm_password");
 
-   
-    let jsonObj = JSON.stringify(personData)
+        function validatePassword() {
+            if (pw.value != confirm_password.value) {
+                confirm_password.setCustomValidity("Passwords Don't Match");
+            } else {
+                confirm_password.setCustomValidity('');
+            }
+        }
+        
+        function hide(){
+            var hides = document.querySelector('.signup_section').style.display="none";
+            
+        }
 
-    localStorage.setItem('Data', jsonObj);
-}
+        function store() {
+
+            var storedData = localStorage.getItem('Data');
+            if(validatePassword()===false){
+                alert("Passwords Don't Match");
+            }
+            else{
+                
+                let obj = {
+                uname: Iname.value,
+                password: pw.value,
+            }
+            personData.push(obj);
+
+            let jsonObj = JSON.stringify(personData)
+
+            localStorage.setItem('Data', jsonObj);
+            }
+        
+        }
+
         function check() {
 
-            var storedData = localStorage.getItem('Data'); 
+            var storedData = localStorage.getItem('Data');
 
             var userName = document.getElementById('username');
             var userPw = document.getElementById('userpw');
 
             console.log(storedData);
-         
-        }
+            var logIn = false;
+            for (i = 0; i < personData.length; i++) {
+                console.log(personData[i].password);
+                if (userName.value === personData[i].uname && userPw.value === personData[i].password) {
+                    logIn = true;
+                } else {
+                    logIn = false;
+                }
+            }
+            if (logIn == true) {
+                alert("Login sucessfull")
+            } else {
+                alert("Incorrrect Password or Username")
+            }
+           
 
-        // When the user clicks anywhere outside of the modal, close it
+
+
+        } 
+
         window.onclick = function (event) {
             if (event.target == modal) {
                 modal.style.display = "none";
